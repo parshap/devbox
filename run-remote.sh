92f8b52e871dd1299ce4b0ae9105a0b59ae26ca4
Parsha Pourkhomami <parshap+git@gmail.com>
2013-12-07 01:48:17 -0800

    Script to run chef on remote host

---
+++ run-remote.sh
@@ -0,0 +1,30 @@
+# Run Chef on a remote host
+# Usage: run-remote.sh parshap@home.parshap.com
+host=$1
+attributes=$(cat attributes.json)
+
+# Remote directory to copy files to
+target=$(ssh $host mktemp -d)
+
+# Clean build files
+rm -rf ./build
+
+# Build cookbooks
+./build.sh
+
+# Copy files to remote host
+echo "Copying cookbooks to $host:$target"
+tar czf - --exclude='.git' . | \
+	ssh $host tar -xzf - --no-same-owner --no-same-permissions -C $target
+
+# Execute on remote host
+ssh $host <<EOF
+	# Change directory to where files were copied
+	cd $target
+
+	# Run chef-solo
+	./solo.sh build '$attributes'
+
+	# Remove files
+	rm -rf $target
+EOF
